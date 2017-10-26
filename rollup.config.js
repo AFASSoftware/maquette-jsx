bca57ec116c7674be0a8e98f3ea7af7083291d14
Rick Hoving <r.hoving@afas.nl>
2017-10-26 14:55:09 +0200

    added tsa scripts to package json

---
+++ rollup.config.js
@@ -0,0 +1,24 @@
+import pkg from './package.json';
+
+// Typescript creates the commonJS package, let rollup do the rest
+
+export default [
+  // browser-friendly UMD build
+  {
+    input: 'dist/index.js',
+    output: {
+      file: pkg.browser,
+      format: 'umd'
+    },
+    name: 'maquetteCssTransitions',
+    plugins: []
+  },
+  // CommonJS build for nodeJS
+  {
+    input: 'dist/index.js',
+    output: {
+      file: pkg.main,
+      format: 'cjs'
+    }
+  }
+];

cd3d7d0cced66c73ea9cb0eae4a376ccab836732
Peter Westendorp <PWE@afasgroep.nl>
2017-10-27 08:56:45 +0200

    splitting up tests

--- test/index-tests.ts
+++ test/index-tests.ts
@@ -1,6 +1,7 @@
 import { enableJsx } from '../src';
 import Global = NodeJS.Global;
 import { expect } from './test-utilities';
+import { VNode } from 'maquette';
 
 describe('maquette-jsx', () => {
   let globalScope: Global & { window?: any } = global;
@@ -17,28 +18,31 @@ describe('maquette-jsx', () => {
     delete (global as any).jsx;
   });
 
-  it('creates VNodes from JSX code', () => {
+  it('registers a jsx function on the global scope', () => {
     enableJsx();
-    (global as any).jsx = window.jsx;
+    expect(window.jsx).to.be.a('function');
+  });
 
-    let vnode = <a href="#">Click me</a>;
-
-    expect(vnode).to.deep.equal({
+  it('creates VNodes from jsx() calls', () => {
+    enableJsx();
+    let vnode = window.jsx('a', { href: '#' }, 'Click me');
+    let expected: VNode = {
+      vnodeSelector: 'a',
       children: [
         {
-          children: undefined,
-          domNode: null,
-          properties: undefined,
-          text: 'Click me',
           vnodeSelector: '',
+          properties: undefined,
+          children: undefined,
+          text: 'Click me',
+          domNode: null
         }
       ],
       domNode: null,
       properties: {
         href: '#'
       },
-      text: undefined,
-      vnodeSelector: 'a'
-    });
+      text: undefined
+    };
+    expect(vnode).to.deep.equal(expected);
   });
 });

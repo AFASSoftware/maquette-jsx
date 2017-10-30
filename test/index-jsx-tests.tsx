beeebbfde023b6b2c906d40ec48737081d5c6214
Johan Gorter <johan-gorter@users.noreply.github.com>
2017-10-30 09:08:02 +0100

    Integration test with jsx

--- test/index-jsx-tests.tsx
+++ test/index-jsx-tests.tsx
@@ -2,6 +2,7 @@ import { enableJsx } from '../src';
 import Global = NodeJS.Global;
 import { expect } from './test-utilities';
 import { dom } from 'maquette';
+require('jsdom-global/register');
 
 describe('maquette-jsx', () => {
   let globalScope: Global & { window?: any } = global;
@@ -52,7 +53,7 @@ describe('maquette-jsx', () => {
         more text
       </p>
       <br />
-      <br class="hello world" />
+      <input class="hello world" />
       <div class="hello world"></div>
       <a href="#">Click me</a>
       <div classes={{special: true}} styles={{height: '100px'}}>
@@ -60,7 +61,20 @@ describe('maquette-jsx', () => {
         w<b>orl</b>d
       </div>
     </div>;
-    // TODO: add JSDom and compare innerHTML
-    //let DOMNode = dom.create(vnode);
+
+    let projection = dom.create(vnode);
+    expect(projection.domNode.outerHTML).to.equal(`
+    <div>
+      <p>text more text</p>
+      <br>
+      <input class="hello world">
+      <div class="hello world"></div>
+      <a href="#">Click me</a>
+      <div class="special" style="height: 100px;">
+        goodbye <br> <i>cruel</i>
+        w<b>orl</b>d
+      </div>
+    </div>
+    `.replace(/\s*\n\s*/g, ''));
   });
 });

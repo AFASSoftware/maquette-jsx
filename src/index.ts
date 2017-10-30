8319226ad668dea046bc48d132a2ae6a048f9446
Johan Gorter <johan-gorter@users.noreply.github.com>
2017-10-30 09:11:37 +0100

    needless remark gone

--- src/index.ts
+++ src/index.ts
@@ -1,7 +1,6 @@
 import { VNode, VNodeProperties } from 'maquette';
 import { JSXElement } from './jsx-types';
 
-// Still missing: text, omission of properties?, non-flattened children?
 export let enableJsx = () => {
   (window as any).jsx = (tagName: string, properties: VNodeProperties | undefined, ...children: (VNode | string)[]): JSXElement => {
     return {

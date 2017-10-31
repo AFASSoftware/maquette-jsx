d44d19e04e954633e58a33697f7005b3acbd8202
Johan Gorter <johan-gorter@users.noreply.github.com>
2017-10-31 14:42:50 +0100

    making sure the global jsx function is recognized by the Typescript compiler

--- src/index.ts
+++ src/index.ts
@@ -1,7 +1,17 @@
+/* tslint:disable no-shadowed-variable */
 import { VNode, VNodeProperties } from 'maquette';
-import { JSXElement } from './jsx-types';
 
-export let jsx = (tagName: string, properties: VNodeProperties | null, ...children: (VNode | string)[]): JSXElement => {
+declare global {
+  function jsx(tagName: string, properties: VNodeProperties | null, ...children: (VNode | string)[]): VNode;
+  namespace JSX {
+    interface IntrinsicElements {
+      [tagName: string]: VNodeProperties;
+    }
+    type Element = VNode;
+  }
+}
+
+export let jsx = (tagName: string, properties: VNodeProperties | null, ...children: (VNode | string)[]): VNode => {
   return {
     vnodeSelector: tagName,
     properties: properties || undefined,
@@ -22,6 +32,9 @@ export let jsx = (tagName: string, properties: VNodeProperties | null, ...childr
   };
 };
 
+/**
+ * Call this function before executing any JSX formatted code. This function makes the window.jsx function available.
+ */
 export let enableGlobalJsx = () => {
   (window as any).jsx = jsx;
 };
